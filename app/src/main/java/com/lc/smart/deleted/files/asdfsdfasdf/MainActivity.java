a3836838fd39eec8146b5ade7dc846f5ac9c1f70
Md Faisal M <faisalchanglah@gmail.com>
2022-08-12 23:01:30 +0500

    asdf

--- app/src/main/java/com/lc/smart/deleted/files/asdfsdfasdf/MainActivity.java
+++ app/src/main/java/com/lc/smart/deleted/files/asdfsdfasdf/MainActivity.java
@@ -12,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 }
